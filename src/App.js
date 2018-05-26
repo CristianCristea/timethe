f4eb6d87fa57696087f7371384aad9f31ef4eb3c
Cristian Cristea <cristiancristea.1987@gmail.com>
2018-05-26 15:44:38 +0200

    Refactor app structure, handle events

--- src/App.js
+++ src/App.js
@@ -1,15 +1,54 @@
-import React, { Component } from 'react';
-import Header from './components/Header';
-import Sidebar from './components/Sidebar';
+import React, { Component } from "react";
+import Header from "./components/Header";
+import CreateProject from "./components/CreateProject";
+import AddProjectModal from "./components/AddProjectModal";
+import Projects from "./components/Projects";
 
 class App extends Component {
-  render() {
-    const navLinks = ['My Projects', 'Finished Projects'];
+  state = {
+    projects: ["test1", "test2"]
+  };
 
+  handleAddProject = project => {
+    console.log("add");
+    if (!project) {
+      return "Enter valid project name";
+    } else if (this.state.projects.indexOf(project) > -1) {
+      return "This project already exists";
+    }
+
+    this.setState(prevState => {
+      return {
+        projects: prevState.projects.concat(project)
+      };
+    });
+  };
+
+  handleDeleteProjects = () => {
+    this.setState(() => ({ projects: [] }));
+  };
+
+  handleDeleteProject = projectToRemove => {
+    this.setState(prevState => ({
+      projects: prevState.projects.filter(
+        project => project !== projectToRemove
+      )
+    }));
+  };
+
+  render() {
     return (
       <div className="app">
-        <Sidebar links={navLinks} />
-        <Header title="My Projects" />
+        <Header
+          title="TIMETHE"
+          subtitle="Track the time you work on projects"
+        />
+        <CreateProject handleAddProject={this.handleAddProject} />
+        <Projects
+          projects={this.state.projects}
+          handleDeleteProjects={this.handleDeleteProjects}
+          handleDeleteProject={this.handleDeleteProject}
+        />
       </div>
     );
   }
