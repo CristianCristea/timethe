eedeb4ebe2c8c86f6777b2f8708216fa94e8c0b8
CristianCristea <cristiancristea.1987@gmail.com>
2018-06-29 22:38:16 +0200

    Basic styles

--- src/components/404/index.jsx
+++ src/components/404/index.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Container, Row, Col, Jumbotron } from 'reactstrap';
 
 const NotFound = () => (
-  <div>
-    <h2 className="display-3">
-      The page you are looking does not exist or has been moved.
-    </h2>
-    <Link to="/" className="btn btn-secondary">
-      Back to dashboard
-    </Link>
-  </div>
+  <Container>
+    <Row>
+      <Col>
+        <Jumbotron className="mt-5 text-center">
+          <h2>
+            The page you are looking does not exist or has been moved.
+          </h2>
+          <Link to="/" className="btn btn-secondary mt-5">
+            Back to dashboard
+          </Link>
+        </Jumbotron>
+      </Col>
+    </Row>
+  </Container>
 );
 
 export default NotFound;
