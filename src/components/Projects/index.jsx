eeca3360fabd6b116b12827b17041b51df5fd2f2
CristianCristea <cristiancristea.1987@gmail.com>
2018-07-01 12:22:26 +0200

    Change to function

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -4,23 +4,23 @@ import { Container, Row, Col } from 'reactstrap';
 import Project from '../Project/index';
 import './projects.css';
 
-const Projects = ({ projects }) => (
-  <section className="projects">
-    <ul className="projects__list p-0">
-      <Container>
-        <Row>
-          {projects.map(project => (
-            <Col xs="12" sm="6" md="4" ls="3" key={project.id}>
-              <Project project={project} />
-            </Col>
-          ))}
-        </Row>
-      </Container>
-    </ul>
-  </section>
-);
-
-export default Projects;
+export default function Projects({ projects }) {
+  return (
+    <section className="projects">
+      <ul className="projects__list p-0">
+        <Container>
+          <Row>
+            {projects.map(project => (
+              <Col xs="12" sm="6" md="4" ls="3" key={project.id}>
+                <Project project={project} />
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      </ul>
+    </section>
+  );
+}
 
 Projects.propTypes = {
   projects: PropTypes.arrayOf(PropTypes.object).isRequired,
