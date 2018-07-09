58b0c31149efeb1d93c8836e63f6432c6de4d2c1
CristianCristea <cristiancristea.1987@gmail.com>
2018-07-09 16:42:22 +0200

    Add initial state

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,10 +2,12 @@ import { createStore, combineReducers } from 'redux';
 import projectsReducer from '../reducers/projects';
 // import filtersReducer from '../reducers/filters';
 
+const initialState = { projects: [] };
+
 export default () => {
   const store = createStore(combineReducers({
     projects: projectsReducer,
-  }), []);
+  }), initialState);
 
   return store;
 };
