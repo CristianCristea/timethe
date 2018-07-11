85b095fab28cfcdba92dd120bd84d809798dae44
CristianCristea <cristiancristea.1987@gmail.com>
2018-07-11 18:54:10 +0200

    Projects reducer

--- src/reducers/projects.js
+++ src/reducers/projects.js
@@ -1,11 +1,25 @@
+import moment from 'moment';
+
 const projectsReducerDefaultState = [];
 
 export default (state = projectsReducerDefaultState, action) => {
   switch (action.type) {
     case 'ADD_PROJECT':
       return [...state, action.project];
-    case 'REMOVE_PROJECT':
-      return state.filter(({ id }) => id !== action.id);
+    case 'DELETE_PROJECT':
+      return state.filter(id => id !== action.id);
+    case 'TOGGLE_PROJECT':
+      return state.map((project) => {
+        if (project.id === action.id) {
+          return {
+            ...project,
+            archived: !action.archived,
+            archivedDates: [...action.archivedDates, moment().format('dddd, DD MM YYYY')],
+          };
+        }
+
+        return project;
+      });
     case 'EDIT_PROJECT':
       return state.map((project) => {
         if (project.id === action.id) {
