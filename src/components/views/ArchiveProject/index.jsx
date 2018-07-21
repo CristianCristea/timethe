ba657267d26d9f1fb75048386c4a281405cc1826
CristianCristea <cristiancristea.1987@gmail.com>
2018-07-21 09:25:06 +0200

    Connect to store, add print and generate pdf, display dates and sessions

--- src/components/views/ArchiveProject/index.jsx
+++ src/components/views/ArchiveProject/index.jsx
@@ -1,25 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import moment from 'moment';
 import { Container, Row, Col, Badge, Button } from 'reactstrap';
+import { handlePrintProject, handleGeneratePDF, formatTime, getTotalSessionsTime, selectProject } from '../../../selectors/projects';
 import MainNav from '../../UI/MainNav';
 import Sessions from '../../Sessions';
 import './ArchiveProject.css';
 
 
-export default function ArchiveProject({
-  project,
-  totalSessionsTime,
-  formatTime,
-  handlePrintProject,
-  handleGeneratePDF,
-}) {
+const ArchiveProject = ({ project }) => {
   const {
     name,
     description,
     startDate,
-    archiveDate,
+    archived,
     sessions,
   } = project;
+  const totalSessionsTime = getTotalSessionsTime(sessions);
+  const startDateFormat = moment.unix(startDate).format('dddd, MMMM Do YYYY');
 
   return (
     <div className="ArchiveProject">
@@ -33,35 +32,47 @@ export default function ArchiveProject({
           </Col>
           <Col>
             <h1 className="mt-4">
-              <Badge color="secondary" className="">{totalSessionsTime}</Badge>
+              <Badge color="secondary" className="">{formatTime(totalSessionsTime)}</Badge>
             </h1>
             <div className="export-btns">
               <Button className="btn btn-success btn-large mb-3" onClick={handlePrintProject}>Print</Button>
-              <Button className="btn btn-success btn-large" onClick={() => handleGeneratePDF(project, totalSessionsTime)}>Export PDF</Button>
+              <Button
+                className="btn btn-success btn-large"
+                onClick={() => handleGeneratePDF(project, totalSessionsTime)}
+              >
+                Export PDF
+              </Button>
             </div>
           </Col>
         </Row>
 
         <Row className="mb-5">
           <Col>
-            <h5>{startDate} - {archiveDate}</h5>
+            <h5>{startDateFormat} - {archived}</h5>
           </Col>
         </Row>
         <Row>
           <Col>
             <h4>Sessions</h4>
-            <Sessions sessions={sessions} formatTime={formatTime} />
+            <Sessions projectName={name} />
           </Col>
         </Row>
-      </Container >
+      </Container>
     </div>
   );
-}
+};
+
+const mapStateToProps = (state, ownProps) => {
+  const projectName = ownProps.match.params.name;
+
+  return {
+    project: selectProject(state.projects, projectName),
+  };
+};
+
+export default connect(mapStateToProps)(ArchiveProject);
+
 
 ArchiveProject.propTypes = {
   project: PropTypes.object.isRequired,
-  totalSessionsTime: PropTypes.string.isRequired,
-  formatTime: PropTypes.func.isRequired,
-  handlePrintProject: PropTypes.func.isRequired,
-  handleGeneratePDF: PropTypes.func.isRequired,
 };
