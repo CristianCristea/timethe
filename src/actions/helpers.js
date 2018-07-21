e8eb11e678d28a01e89832aed5e1ecb6b8f127da
Cristian Cristea <cristiancristea.1987@gmail.com>
2018-07-21 09:09:29 +0200

    Style code

--- src/actions/helpers.js
+++ src/actions/helpers.js
@@ -1,4 +1,4 @@
-export const toggleIsSessionActive = (isSessionActive) => ({
+export const toggleIsSessionActive = isSessionActive => ({
   type: 'TOGGLE_IS_SESSION_ACTIVE',
   isSessionActive,
 });
