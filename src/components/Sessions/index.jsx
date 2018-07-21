3902544144be21bcd1023880ca308ed3d5ca4cdd
CristianCristea <cristiancristea.1987@gmail.com>
2018-07-21 09:11:14 +0200

    Connect to store

--- src/components/Sessions/index.jsx
+++ src/components/Sessions/index.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 import uuid from 'uuid';
 import PropTypes from 'prop-types';
 import { Table } from 'reactstrap';
+import { connect } from 'react-redux';
+import { selectProject, formatTime } from '../../selectors/projects';
 import Session from '../Session';
 
-export default function Sessions({ sessions, formatTime }) {
+const Sessions = ({ currentProject }) => {
+  const { sessions } = currentProject;
+
   return (
     <div className="table-responsive">
       <Table striped>
@@ -26,12 +30,22 @@ export default function Sessions({ sessions, formatTime }) {
   );
 }
 
-Sessions.propTypes = {
-  sessions: PropTypes.arrayOf(PropTypes.shape({
-    date: PropTypes.string,
-    note: PropTypes.string,
-    seconds: PropTypes.number,
-  })).isRequired,
-  formatTime: PropTypes.func.isRequired,
-}
+const mapStateToProps = (state, ownProps) => ({
+  currentProject: selectProject(state.projects, ownProps.projectName),
+});
+export default connect(mapStateToProps)(Sessions);
 
+Sessions.propTypes = {
+  projectName: PropTypes.string.isRequired,
+  currentProject: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    sessions: PropTypes.arrayOf(PropTypes.shape({
+      date: PropTypes.string,
+      note: PropTypes.string,
+      seconds: PropTypes.number,
+    })),
+    startDate: PropTypes.number.isRequired,
+  }).isRequired,
+};
