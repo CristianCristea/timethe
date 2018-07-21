29d9e871ffe6ad8961985a06a571cbb78fdd62e8
Cristian Cristea <cristiancristea.1987@gmail.com>
2018-07-21 19:20:52 +0200

    Add test data

---
+++ src/fixtures/projects.js
@@ -0,0 +1,37 @@
+import moment from 'moment';
+
+export default [
+  {
+    id: '96238fca-d13e-409e-845a-07364a5c4c14',
+    name: 'alibaba.com',
+    description: 'bulk ware seller',
+    sessions: [],
+    startDate: moment(0).add(10, 'days').valueOf(),
+    archived: '',
+  },
+  {
+    id: '96238fca-d13e-409e-845a-07364a5c4c15',
+    name: 'google.com',
+    description: 'search engine',
+    sessions: [],
+    startDate: moment(0).add(5, 'days').valueOf(),
+    archived: '',
+  },
+  {
+    id: '96238fca-d13e-409e-845a-07364a5c4c16',
+    name: 'paypal.com',
+    description: 'online pay system',
+    sessions: [],
+    startDate: moment(0).add(4, 'days').valueOf(),
+    archived: '',
+  },
+  {
+    id: '96238fca-d13e-409e-845a-07364a5c4c17',
+    name: 'amazon.com',
+    description: 'online marketplace',
+    sessions: [],
+    startDate: moment(0).add(1, 'days').valueOf(),
+    archived: '',
+  },
+
+];
