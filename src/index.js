2c70fce8db32c246b3693869f60a9216549839b8
CristianCristea <cristiancristea.1987@gmail.com>
2018-07-27 13:50:22 +0200

    Add test and dev .env, loading screen

--- src/index.js
+++ src/index.js
@@ -1,18 +1,34 @@
+/* global document */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import dotenv from 'dotenv';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
+import { startSetProjects } from './actions/projects';
 import App from './App';
 import configureStore from './store/configureStore';
 import registerServiceWorker from './registerServiceWorker';
 
-const store = configureStore();
+// set the values for firebase config based on the env test or develoopment
+if (process.env.NODE_ENV === 'development') {
+  dotenv.config({ path: '../.env.development' });
+} else if (process.env.NODE_ENV === 'test') {
+  dotenv.config({ path: '../.env.test' });
+}
 
-ReactDOM.render(
+const store = configureStore();
+const jsx = (
   <Provider store={store}>
     <App store={store} />
-  </Provider>,
-  document.getElementById('root'),
+  </Provider>
 );
+
+ReactDOM.render(<p>Loading...</p>, document.getElementById('root'));
+
+store.dispatch(startSetProjects()).then(() => {
+  ReactDOM.render(jsx, document.getElementById('root'));
+});
+
 registerServiceWorker();
