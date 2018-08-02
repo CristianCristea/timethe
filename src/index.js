1742be3d524f9fc9b58782a376387822a9b0fec9
Cristian Cristea <cristiancristea.1987@gmail.com>
2018-08-02 19:18:56 +0200

    Remove dotenv config, add firebase auth, redirect after login, logout

--- src/index.js
+++ src/index.js
@@ -3,32 +3,59 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import dotenv from 'dotenv';
+// import dotenv from 'dotenv';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
-import { startSetProjects } from './actions/projects';
-import App from './App';
+import { firebase } from './firebase/firebase';
 import configureStore from './store/configureStore';
+import AppRouter, { history } from './routes/AppRouter';
+import { startSetProjects } from './actions/projects';
+import { login, logout } from './actions/auth';
+import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
 // set the values for firebase config based on the env test or develoopment
-if (process.env.NODE_ENV === 'development') {
-  dotenv.config({ path: '../.env.development' });
-} else if (process.env.NODE_ENV === 'test') {
-  dotenv.config({ path: '../.env.test' });
-}
+// if (process.env.NODE_ENV === 'development') {
+//   dotenv.config({ path: '../.env.development' });
+// } else if (process.env.NODE_ENV === 'test') {
+//   dotenv.config({ path: '../.env.test' });
+// }
 
 const store = configureStore();
 const jsx = (
   <Provider store={store}>
-    <App store={store} />
+    <AppRouter store={store} />
   </Provider>
 );
+let hasRendered = false;
+const renderApp = () => {
+  // render the app one single time - only if is not already rendered
+  if (!hasRendered) {
+    ReactDOM.render(jsx, document.getElementById('root'));
+    hasRendered = true;
+  }
+};
 
 ReactDOM.render(<p>Loading...</p>, document.getElementById('root'));
 
-store.dispatch(startSetProjects()).then(() => {
-  ReactDOM.render(jsx, document.getElementById('root'));
-});
-
 registerServiceWorker();
+
+// firebase auth
+// user.uid is provided by firebase
+// dispatch actions based on user login status
+// if logged in store the user id in store
+// redirect after login or logout
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    store.dispatch(login(user.uid));
+    store.dispatch(startSetProjects()).then(() => {
+      renderApp();
+      if (history.location.pathname === '/') {
+        history.push('/dashboard');
+      }
+    });
+  } else {
+    store.dispatch(logout());
+    renderApp();
+    history.push('/');
+  }
+});
