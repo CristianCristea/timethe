03fd9a78daf9aff8894d7b438da3c87ea8df7c7a
CristianCristea <cristiancristea.1987@gmail.com>
2018-08-02 19:25:20 +0200

    Fix import path

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Container, Row, Col } from 'reactstrap';
-import Project from '../Project/index';
+import Project from '../Project';
 import './projects.css';
 
 function Projects({ projects }) {
