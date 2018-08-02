34d59b77701a6a1bbc9de68af3d799872df0bccb
CristianCristea <cristiancristea.1987@gmail.com>
2018-08-02 20:20:00 +0200

    Restyle projects list

--- src/components/Project/index.jsx
+++ src/components/Project/index.jsx
@@ -1,21 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import moment from 'moment';
 import { Link } from 'react-router-dom';
 import { Card, CardBody, CardTitle, CardText } from 'reactstrap';
 import './project.css';
 
 export default function Project({ project }) {
-  const link = project.archived
-    ? `/archive/${project.name}`
-    : `/projects/${project.name}`;
+  const {
+    name,
+    description,
+    startDate,
+    archived,
+  } = project;
+  const link = archived
+    ? `/archive/${name}`
+    : `/projects/${name}`;
 
   return (
     <li className="project">
       <Link to={link} className="project-link" >
         <Card className="project-card">
-          <CardBody className="project-card-body">
-            <CardTitle tag="h4" className="project-card-title">{project.name}</CardTitle>
-            <CardText>{project.description}</CardText>
+          <CardBody className="project__card-body">
+            <div>
+              <CardTitle tag="h4" className="project__card-title">{name}</CardTitle>
+              <div className="project__date">{moment.unix(startDate).format('MMMM Do YYYY')}</div>
+            </div>
+            <CardText>{description}</CardText>
           </CardBody>
         </Card>
       </Link>
--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Container, Row, Col } from 'reactstrap';
+import { Container, Row, Col, Table } from 'reactstrap';
 import Project from '../Project';
 import './projects.css';
 
 function Projects({ projects }) {
   return (
-    <section className="projects">
+    <section>
       <ul className="projects__list p-0">
         <Container>
           <Row>
-            {projects.map(project => (
-              <Col xs="12" sm="6" md="4" ls="3" key={project.id}>
-                <Project project={project} />
-              </Col>
-            ))}
+            <Col>
+              <div className="projects">
+                {projects.map(project => (
+                  <Project project={project} key={project.id} />
+                ))}
+              </div>
+            </Col>
           </Row>
         </Container>
       </ul>
